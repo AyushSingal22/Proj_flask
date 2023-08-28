9fc1b4f26ba658845763b17700cc8b3f3bf0ebf7
Ayush Singal <ayushsingal22@gmail.com>
2023-08-28 14:21:06 +0530

    ad

--- connection.py
+++ connection.py
@@ -11,19 +11,7 @@ account = 'wc50696.us-east-2.aws'
 warehouse = 'compute_wh'
 database = 'COVID19_EPIDEMIOLOGICAL_DATA'
 schema = 'public'
-# Create a connectionc
-# class MongoAPI:
-#     def __init__(self):
-        
-#         uri = "mongodb+srv://<>:<password>@cluster0.8knjyug.mongodb.net/?retryWrites=true&w=majority"
-#         # Create a new client and connect to the server
-#         client = MongoClient(uri, server_api=ServerApi('1'))
-#         # Send a ping to confirm a successful connection
-#         try:
-#             client.admin.command('ping')
-#             print("Pinged your deployment. You successfully connected to MongoDB!")
-#         except Exception as e:
-#             print(e)
+
 class Api_data_provider:
     def __init__(self):
         self.conn = snowflake.connector.connect(
@@ -52,7 +40,7 @@ class Api_data_provider:
        
        
        
-        plt.hist(df.head(10))
+        plt.hist(df)
         
         
         
